ffb6da370f3c823bcd5c56164c0c73d29ea4c872
Eduardo-Montanhani <montanhani81@gmail.com>
2023-06-02 23:21:38 -0300

    coloquei textarea no form.php

--- form.php
+++ form.php
@@ -5,22 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>formulario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
+    
     <form action="recuperar-dados.php" method="post">
-        <label for="nome">NOME</label>
+        <label for="nome" class="form-label">NOME</label>
         <input type="text" name="nome" id="nome" placeholder="Digite o nome!">
         
-        <label for="email">EMAIL</label>
+        <label for="email" class="form-label">EMAIL</label>
         <input type="email" name="email" id="email" placeholder="Coloque o email!">
         
-        <label for="idade">IDADE</label>
+        <label for="idade" class="form-label">IDADE</label>
         <input type="number" name="idade" id="iadade" placeholder="Digite sua idade!">
        
-        <label for="hobbie">HOBBIE</label>
-        <input type="text" name="hobbie" id="hobbie" placeholder="Esceeva seu hobbie!">
+        <label for="hobbie" class="form-label">HOBBIE</label>
+        <textarea  type="text" name="hobbie" id="hobbie" placeholder="Digite sua mensagem"></textarea>
        
-        <button type="submit">Enviar</button>
+        <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
